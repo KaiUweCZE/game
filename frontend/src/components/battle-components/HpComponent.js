5d9437ef6b5c6c71601297d18fdc7fbae8b4053e
KaiUweCZE <pisezdenekpise@seznam.cz>
2023-09-12 19:46:59 +0200

    sketch of hp component

--- frontend/src/components/battle-components/HpComponent.js
+++ frontend/src/components/battle-components/HpComponent.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const HpComponent = () => {
+    const [actualHp, setActualHp] = useState(100)
     /*funcs: how many hp does a pokemon have? */
+    // attack and hp will be parameters
+    const hp = 100
+    const attack = 10
+    
 
-    return(
+    //sketch of a attack func
+    const handleAttack = () => {
+        console.log(attack);
+        const damage = actualHp - attack
+        setActualHp(damage)
+    }
+
+
+    return (
+        <>
         <div className="hp">
-            <div className="your-hp">
+            <div className="your-hp" style={{ width: `${actualHp}%` }}>
             </div>
         </div>
+        <button onClick={handleAttack}>attack</button>
+        </>
     )
 }
 
