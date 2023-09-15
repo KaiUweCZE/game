d3bb27427abd1bf3826794dfea4320325eaec920
Kai Uwe <pisezdenekpise@seznam.cz>
2023-09-15 18:30:43 +0200

    reconfig BoxAttacks behavior 2.0

--- frontend/src/components/battle-components/BoxAttacks.js
+++ frontend/src/components/battle-components/BoxAttacks.js
@@ -5,19 +5,20 @@ import { handlerAttack, handlerSwitch, handlerAvoid, reloadAttack } from "./batt
 const BoxAttacks = (props) => {
     const [ currentAttack, setCurrentAttack ] = useState({})
     const [clickCounter, setClickCounter] = useState(0);
+    const [clickable, setClickable ] = useState(true)
     const [reload, setReload] = useState(0);
+    const [duration, setDuration] = useState(0)
     const [displayReload, setDisplayReload] = useState(0);
     const pokemon = props.id.pokemon
     const attacks = pokemon.attacks
   
     // attack reload
-   
     useEffect(() => {
         const dmg = currentAttack.dmg;
+        // only because of editing * 3 
         const reload = currentAttack.reload;
         setReload(reload);
         props.updateDmg(dmg);
-        setClickCounter(0)
     }, [currentAttack, clickCounter])
 
 
@@ -30,14 +31,19 @@ const BoxAttacks = (props) => {
     const updateReload = () => {
         const timout = (Date.now() - start) / 1000;
         const remaining = Math.max(0, reload - timout);
-        
+
+       setDuration(reload) 
         setDisplayReload(remaining);
+        //console.log("animation duration is: ", remaining);
         if (remaining > 0) {
             timer = setTimeout(updateReload, 10);
+        } else{
+            setClickable(true)
         }
     };
 
     if (reload > 0) {
+        setClickable(false)
         updateReload();
     }
     
@@ -47,9 +53,7 @@ const BoxAttacks = (props) => {
     const handleAttackClick = (attack) => {
         const handledAttack = handlerAttack(attack);
         setCurrentAttack(handledAttack);
-        //setReload(handledAttack.reload);
         setClickCounter(prevCount => prevCount + 1);
-        //props.updateDmg(handledAttack.dmg);
     };
 
     return(
@@ -57,13 +61,13 @@ const BoxAttacks = (props) => {
             {
                 attacks.map((attack, index) => {
                     return(
-                        <li key={index} className="attacks" onClick={() => handleAttackClick(attack)}>{attack.name ? attack.name : "" }</li>
+                        <li key={index} className="attacks" onClick={clickable ? () => handleAttackClick(attack) : null}>{attack.name ? attack.name : "" }</li>
                     )
                 })
             }
             <li className="avoid" onClick={handlerAvoid}>avoid!</li>
             <li className="switch" onClick={handlerSwitch}>switch</li>
-            <h2>{displayReload.toFixed(2)}</h2>
+            <li className={`reloader ${clickable ? "" : "deactive"}`} style={clickable ? {} : { "--duration": `${duration}s`}}>{displayReload.toFixed(2)}</li>
         </ul>
         
     )
